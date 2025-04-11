ebdea8cdc3768f64e4ee7c582bffee2dada8794c
MaykonAlves <MaykonAlves@DESKTOP-B3VDBQE>
2025-04-11 01:30:39 -0300

    Adicionar arquivos de projeto.

---
+++ Cliente_Texto/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Cliente_Texto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.lstMensagens = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPorta = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.txtMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMensagem.Location = new System.Drawing.Point(164, 164);
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(318, 21);
+            this.txtMensagem.TabIndex = 0;
+            // 
+            // lstMensagens
+            // 
+            this.lstMensagens.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lstMensagens.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstMensagens.FormattingEnabled = true;
+            this.lstMensagens.ItemHeight = 15;
+            this.lstMensagens.Location = new System.Drawing.Point(164, 235);
+            this.lstMensagens.Name = "lstMensagens";
+            this.lstMensagens.Size = new System.Drawing.Size(318, 139);
+            this.lstMensagens.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(559, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Porta";
+            // 
+            // txtPorta
+            // 
+            this.txtPorta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.txtPorta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPorta.Location = new System.Drawing.Point(563, 232);
+            this.txtPorta.Name = "txtPorta";
+            this.txtPorta.Size = new System.Drawing.Size(100, 21);
+            this.txtPorta.TabIndex = 3;
+            this.txtPorta.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPorta_KeyPress);
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnConectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.Location = new System.Drawing.Point(563, 319);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(100, 30);
+            this.btnConectar.TabIndex = 4;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = false;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click_1);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(164, 390);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(100, 30);
+            this.btnEnviar.TabIndex = 5;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = false;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(559, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "IP";
+            // 
+            // txtIP
+            // 
+            this.txtIP.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.txtIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIP.Location = new System.Drawing.Point(563, 186);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(100, 21);
+            this.txtIP.TabIndex = 7;
+            this.txtIP.Text = "127.0.0.1";
+            this.txtIP.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtIP_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(162, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Cliente 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(162, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Mensagem";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(559, 258);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Usuário";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(563, 280);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 21);
+            this.txtUsuario.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(162, 212);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Chat";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Cliente_Texto.Properties.Resources.azul;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtPorta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstMensagens);
+            this.Controls.Add(this.txtMensagem);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMensagem;
+        private System.Windows.Forms.ListBox lstMensagens;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPorta;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Cliente_Texto/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cliente_Texto
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient cliente;
+        private StreamWriter escritor;
+        private StreamReader leitor;
+        private String usuario ="";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private async Task EscutarServidor()
+        {
+            // loop infinito pra sempre escutar o servidor
+            while (true)
+            {
+                try
+                {
+                    //pega o que servidor mandou e armazena numa variável do tipo string ( Readlineasync)
+                    string mensagem = await leitor.ReadLineAsync();
+
+                    //se a mensagem não for nula
+                    if (mensagem != null)
+                    {
+                        //adiciona a mensagem no listbox
+                        lstMensagens.Invoke(new Action(() => lstMensagens.Items.Add($"{mensagem}")));
+                    }
+                }
+                catch
+                {
+                    //Caso não consiga conversar com servidor , ele eh descontado
+                    lstMensagens.Invoke(new Action(() => lstMensagens.Items.Add("Desconectado do servidor.")));
+                    break;
+                }
+            }
+        }
+
+
+
+
+
+        private void btnEnviar_Click_1(object sender, EventArgs e)
+        {
+            //caso a mensagem não seja vazia ou com espaços branco
+            if (!string.IsNullOrWhiteSpace(txtMensagem.Text))
+            {
+                //verifica se o usuário é válido, caso não seja , pede pra se conectar no servidor
+                if(usuario == "")
+                {
+                    MessageBox.Show("Por Favor conecte-se e coloque um nome de usuário válido");
+                    txtUsuario.Focus();
+                }
+                else {
+                    //usa o Writeline ,armazena em memória ele não envia de uma vez;
+                    escritor.WriteLine(txtMensagem.Text);
+                    //o Flush ele envia pra o servidor, e apaga os arquivos da memória
+                    escritor.Flush();
+                    //escreve no listbox a mensagem que você acabou enviar ( só com Você) na frente da mensagem
+                    lstMensagens.Items.Add($"Você: {txtMensagem.Text}");
+                    txtMensagem.Clear();
+                }
+            }
+        }
+
+
+
+        private void btnConectar_Click_1(object sender, EventArgs e)
+        {
+            //verifica se tudo foi digitado corretamente
+            if (!(string.IsNullOrEmpty(txtUsuario.Text)) && !(string.IsNullOrEmpty(txtIP.Text)) && !(string.IsNullOrEmpty(txtPorta.Text)))
+            {
+                cliente = new TcpClient();
+                string ip = txtIP.Text;
+                int porta = int.Parse(txtPorta.Text);
+
+                //tenta conectar com servidor
+                try
+                {
+                    cliente.Connect(ip, porta);
+                    lstMensagens.Items.Add("Conectado ao servidor!");
+                    //cria a variável do Stream que vai receber um Network stream como dito no Servidor 2 serve pra estabelecer um canal de comunicação entre o cliente e o servidor
+                    var stream = cliente.GetStream(); 
+                    //escritor permite que o cliente possa escrever pra o servidor
+                    escritor = new StreamWriter(stream, Encoding.UTF8, 999, leaveOpen: true);
+                    // leitor permite que cliente possa receber dados do servidor
+                    leitor = new StreamReader(stream, Encoding.UTF8);
+
+                    //quando acaba de conectar com o servidor , ele vai enviar uma mensagem , essa primeira mensagem , o servidor espera que seja o nome do cliente ( que será a variável usuário , que é referente o que o usuário digitou no txtUsuário)
+                    usuario = txtUsuario.Text;
+                    escritor.WriteLine(usuario);
+                    escritor.Flush();
+
+                   
+                    txtUsuario.Enabled = false;
+                    btnConectar.Enabled = false;
+
+                   //executa o método que eu expliquei acima de escutar servidor , como disse anteriormente task serve para executar o método de forma asyncrona , numa thread separada
+                    Task.Run(() => EscutarServidor());
+                }
+                catch (Exception ex)
+                {
+                    lstMensagens.Items.Add($"Erro ao conectar: {ex.Message}");
+                }
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(txtIP.Text))
+                {
+                    MessageBox.Show("Por favor digite o IP");
+                    txtIP.Focus();
+                }
+                else if (string.IsNullOrEmpty(txtPorta.Text))
+                {
+                    MessageBox.Show("Por favor digite a porta");
+                    txtPorta.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Por favor digite o nome do usuário");
+                    txtUsuario.Focus();
+                }
+            }
+        }
+        //serve pra que o usuário só digite números no campo IP e Porta
+        private void txtIP_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Program.IntNumber(e);
+        }
+
+        private void txtPorta_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Program.IntNumber(e);
+        }
+    }
+}
+
